9b5f349463150987ab5d25b2bac1027d963515cf
Gabriel <gabrieljimmy@gmail.com>
2024-03-18 18:20:56 +0700

    first commit

--- latihan1/app/Http/Controllers/KategoriController.php
+++ latihan1/app/Http/Controllers/KategoriController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Kategori;
+use Barryvdh\DomPDF\Facade\Pdf;
 
 class KategoriController extends Controller
 {
@@ -65,5 +66,11 @@ class KategoriController extends Controller
         }catch (\Exception $e){
             return redirect(route('kategori.index'))->with('pesan', ['danger', 'Gagal hapus  kategori']);
         }
+    }
+
+    public function exportPdf(){
+        $kategori = Kategori::all();
+        $pdf = Pdf::loadView('backend.content.kategori.export', compact('kategori'));
+        return $pdf->download('Data Kategori.pdf');
     }
 }
